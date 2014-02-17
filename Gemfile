e8a918dcde3d6f7e8647ea2cc15deaef0caa2dbe
Michael Prilop <Michael.Prilop@gmail.com>
2014-02-17 19:31:58 +0100

    Heroku compatibility

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+ruby '2.0.0'
+
 gem 'rails', '~>3.2' # otherwise 0.9.2 is installed
 gem 'sqlite3', :group => :development
 gem 'pg'
--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Direktkreditverwaltung
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Prevent initializing app to make precompile task run on heroku
+    config.assets.initialize_on_precompile = false
   end
 end
