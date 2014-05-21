dc9ed1ae169bac6e88d55aadc20f3e7d2499c852
Michael Prilop <Michael.Prilop@gmail.com>
2014-05-21 15:15:04 +0200

    helpers to display short lines for contact or contract

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -1,2 +1,15 @@
 module ContactsHelper
+  def contact_short(contact)
+    return 'no contact' unless contact
+    contact_line = contact.name
+    contact_line << ", #{contact.prename}" unless contact.prename.blank?
+    contact_line
+  end
+
+  def contact_long(contact)
+    contact_line = contact_short(contact)
+    contact_line << " (#{contact.email}" unless contact.email.blank?
+    contact_line << " [#{contact.address}]" unless contact.address.blank?
+    contact_line
+  end
 end
--- app/helpers/contracts_helper.rb
+++ app/helpers/contracts_helper.rb
@@ -1,7 +1,11 @@
 module ContractsHelper
 
   def contract_to_line(contract)
-    "#{contract.number} - #{contract.contact.try(:name)} - #{currency contract.balance} - #{fraction contract.interest_rate}"
+    contract_line = "##{contract.number}"
+    contract_line << " - #{contact_short(contract.contact)}"
+    contract_line << " - #{currency contract.balance}@#{fraction contract.interest_rate}"
+    contract_line << " - X(#{contract.terminated_at})" if contract.terminated_at
+    contract_line
   end
 
 end
