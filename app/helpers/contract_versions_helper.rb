23cfa7dd3eac88739d493dd4ddd480b28825cb5d
Michael Prilop <Michael.Prilop@gmail.com>
2014-05-21 15:51:25 +0200

    showing versions and accounting entries on contract page

--- app/helpers/contract_versions_helper.rb
+++ app/helpers/contract_versions_helper.rb
@@ -1,2 +1,8 @@
 module ContractVersionsHelper
+  def version_as_short_line(version)
+    version_line = "##{version.version}"
+    version_line << " seit #{version.start}"
+    version_line << " verzinsung #{fraction version.interest_rate}"
+    version_line
+  end
 end
