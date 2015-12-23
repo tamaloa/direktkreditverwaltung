00b9863a020a5908c9eac55a9f6398851e588615
Kristine <kristine@artcom.de>
2015-12-23 19:08:44 +0100

    Fix overwriting of contact name

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -1,7 +1,7 @@
 module ContactsHelper
   def contact_short(contact)
     return 'no contact' unless contact
-    contact_line = contact.name
+    contact_line = contact.name.dup
     contact_line << ", #{contact.prename}" unless contact.prename.blank?
     contact_line
   end
@@ -9,8 +9,8 @@ module ContactsHelper
   def contact_long(contact)
     contact_line = contact_short(contact)
     return contact_line unless contact
-    contact_line << " (#{contact.email}" unless contact.email.blank?
-    contact_line << " [#{contact.address}]" unless contact.address.blank?
+    contact_line << " | #{contact.email}" unless contact.email.blank?
+    contact_line << " | #{contact.address}" unless contact.address.blank?
     contact_line
   end
 end
