ef934e58f79e1aaebe2a31ed06ea3c20804a06f8
Michael Prilop <Michael.Prilop@gmail.com>
2016-02-14 15:15:49 +0100

    moved test files to new rails4 directory structure

--- test/controllers/companies_controller_test.rb
+++ test/controllers/companies_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class CompaniesControllerTest < ActionController::TestCase
   setup do
-    @company = Company.first
+    @company = Company.find(1)
   end
 
   test "should get index" do
@@ -11,10 +11,7 @@ class CompaniesControllerTest < ActionController::TestCase
     assert_not_nil assigns(:companies)
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
+
 
   test "should create company" do
     assert_difference('Company.count') do
@@ -35,7 +32,7 @@ class CompaniesControllerTest < ActionController::TestCase
   end
 
   test "should update company" do
-    patch :update, id: @company, company: { bank_account_info: @company.bank_account_info, bank_name: @company.bank_name, city: @company.city, email: @company.email, gmbh_executive_board: @company.gmbh_executive_board, gmbh_name: @company.gmbh_name, gmbh_register_number: @company.gmbh_register_number, gmbh_tax_number: @company.gmbh_tax_number, name: @company.name, street: @company.street, verein_name: @company.verein_name, web: @company.web, zip_code: @company.zip_code }
+    put :update, id: @company, company: { bank_account_info: @company.bank_account_info, bank_name: @company.bank_name, city: @company.city, email: @company.email, gmbh_executive_board: @company.gmbh_executive_board, gmbh_name: @company.gmbh_name, gmbh_register_number: @company.gmbh_register_number, gmbh_tax_number: @company.gmbh_tax_number, name: @company.name, street: @company.street, verein_name: @company.verein_name, web: @company.web, zip_code: @company.zip_code }
     assert_redirected_to company_path(assigns(:company))
   end
 
--- test/unit/helpers/contract_versions_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class ContractVersionsHelperTest < ActionView::TestCase
-end
