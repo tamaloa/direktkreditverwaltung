99a8344dd0728b29a30b44fe8966bbc0e56bc1f7
Michael Prilop <Michael.Prilop@uni-jena.de>
2020-01-27 09:41:29 +0100

    configure email sending via smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,4 +84,15 @@ Direktkreditverwaltung::Application.configure do
     [u, p] == [ENV['HTTP_BASIC_AUTH_USER'], ENV['HTTP_BASIC_AUTH_PASSWORD'] || SecureRandom.hex]
   end
 
+  # Send Emails via SMTP - configure via Environment
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :address              => ENV['SMTP_ADDRESS'],
+  :port                 => ENV['SMTP_PORT'],
+  :domain               => ENV['SMTP_DOMAIN'],
+  :user_name            => ENV['SMTP_USER_NAME'],
+  :password             => ENV['SMTP_PASSWORD'],
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  }
+  
 end
